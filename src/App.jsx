13f5c10ab7faf7bc7fe88947860d394840492d75
Deepak <sinhadeepak619@gmail.com>
2025-02-14 21:36:49 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { HeroSection } from "./components/HeroSection";
+import { Navbar } from "./components/Navbar";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <HeroSection></HeroSection>
+    </>
+  );
+};
+
+export default App
